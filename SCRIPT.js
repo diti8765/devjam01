da7e1e03c08b63a2934d44f3cdb8f6c88e6313d1
Tauqeer52521 <127301063+Tauqeer52521@users.noreply.github.com>
2023-03-16 21:46:14 +0530

    Update SCRIPT.js

--- SCRIPT.js
+++ SCRIPT.js
@@ -1,39 +1,12 @@
-let input = document.getElementById('inputbox');
-let buttons = document.querySelectorAll('button');
-
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('Click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
-    
-    
-     else if(e.target.innerHTML == 'AC'){
-        string = "";
-        input.value = string;
-    }
-    else if(e.target.innerHTML == 'DEL'){
-    string = string.substring(0, string.length-1);
-    input.value = string;
-    }
-    else{
-        string += e.target.innerHTML;
-        input.value = string;
-    }
-    )
-})
-    
+//this function ensure the input from calculator shown on screen    
 function clk(val){
 document.getElementById("inputbox").value=document.getElementById("inputbox").value+val;
 }    
-    
+//clear the screen    
 function clrdisp(){
 document.getElementById("inputbox").value=" ";
 }    
-
+//manipulate the expression
 function eql(){
 var text=document.getElementById("inputbox").value;
     var result=eval(text);
